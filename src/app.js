b5c8218282377916c1b7318f0a496eec465c5cab
Bien Thanh Chung <bienthanhchung@luvina.net>
2020-11-11 15:40:29 +0700

    Done

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-const template = <p>Oois me asdasdoi chans vcl</p>
-ReactDOM.render(template, document.getElementById('root'));
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
