19a1062e44cfde01eeef53babdc40fa14641e5d5
Chung Biện <chungch251997@gmail.com>
2020-11-13 01:35:46 +0700

    Done create add Word

--- src/app.js
+++ src/app.js
@@ -1,30 +1,212 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "antd/dist/antd.css";
+import { Input, Button, Row, Col, Select } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { ToastsContainer, ToastsStore } from "react-toasts";
 
+const { Option } = Select;
 
 function App() {
+  const url = "http://localhost:1325";
+  const quotes = [
+    {
+      id: 0,
+      quote: "Sói thì đi săn chứ đéo cần cho ăn.",
+    },
+    {
+      id: 1,
+      quote: "Sư tử và hổ thì đều diễn xiếc. Sói thì đéo",
+    },
+    {
+      id: 2,
+      quote: "Con quỷ không ở dưới gầm giường. Nó ở trong mày",
+    },
+  ];
+  const [word, setWord] = useState({
+    text: null,
+    phonetic: null,
+    meaning: null,
+    sentence: null,
+    translate: null,
+  });
+  const [units, setUnits] = useState([]);
+  const [unitSelectId, setUnitSelectId] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    let newWord = { ...word };
+    newWord = Object.assign(newWord, { [e.target.id]: e.target.value });
+    setWord(newWord);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`${url}/units`)
+      .then(function (response) {
+        console.log(response);
+        setUnits(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const handleChangeUnit = (value) => {
+    setUnitSelectId(value);
+  };
+
+  const handleSubmit = () => {
+    console.log(units);
+    const unitSelect = units.filter((u) => u.id === unitSelectId)[0];
+    let isComplete = true;
+    if (!unitSelect) {
+      ToastsStore.error("I dont have Unit 0! Are u ok?");
+      return;
+    }
+    Object.values(word).map((value) => {
+      if (!value) {
+        isComplete = false;
+        return;
+      }
+    });
+    if (!isComplete) {
+      ToastsStore.error(
+        "Screen has 5 fields! You must have filled out all of them!"
+      );
+      return;
+    }
+    const wordInUnit = unitSelect.words.find(
+      (w) =>
+        word.text.toLowerCase() === w.text.toLowerCase() ||
+        word.phonetic.toLowerCase() === w.phonetic.toLowerCase()
+    );
+    if (wordInUnit) {
+      ToastsStore.error("Hey! I got it! Check your fucking WORD");
+      return;
+    } else {
+      setLoading(true);
+      const { text, phonetic, meaning, sentence, translate } = word;
+      unitSelect.words.push({
+        id: unitSelect.words.length + 1,
+        text,
+        phonetic,
+        meaning,
+        sentence,
+        translate,
+      });
+      axios
+        .put(`${url}/units/${unitSelect.id}`, unitSelect)
+        .then(function (response) {
+          // Đoạn này đáng nhẽ phải clone units và set lại word.
+          // Trick vcl
+          units.filter((u) => u.id === response.data.id)[0][`words`] =
+            response.data.words;
+          setLoading(false);
+          ToastsStore.success("Good Job bro!");
+          setWord({
+            text: null,
+            phonetic: null,
+            meaning: null,
+            sentence: null,
+            translate: null,
+          });
+          Object.keys(word).map((k) => (document.getElementById(k).value = ""));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
+
+  const renderUnits = () => {
+    return (
+      <>
+        {units.map((unit) => (
+          <Option
+            value={unit.id}
+            disabled={unit && unit.words && unit.words.length === 30}
+          >
+            {`${unit.id} - ${unit.name}`}
+          </Option>
+        ))}
+      </>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="card">
+      <div className="card__title word">Lorem ipsum dolor sit</div>
+      <div className="card__form">
+        <Row>
+          <Col span={12}>
+            <Select
+              style={{ width: "80%" }}
+              defaultValue="0 - Select Unit"
+              onChange={handleChangeUnit}
+            >
+              {renderUnits()}
+            </Select>
+          </Col>
+          <Col span={12}>{`"${quotes[unitSelectId % 3].quote}"`}</Col>
+        </Row>
+      </div>
+      <div className="card__content">
+        <Input
+          className="content__input"
+          id="text"
+          placeholder="New word - Từ mới"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <Input
+          className="content__input"
+          id="phonetic"
+          placeholder="Phonetic - Phiên âm"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <Input
+          className="content__input"
+          id="meaning"
+          placeholder="Meaning of word - Ý nghĩa"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <Input
+          className="content__input"
+          id="sentence"
+          placeholder="Sentence - Câu văn"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <Input
+          className="content__input"
+          id="translate"
+          placeholder="Translate - Dịch câu"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+      </div>
+      <div className="card-action card-action__select">
+        <Button
+          type="primary"
+          icon={<DownloadOutlined />}
+          size={"large"}
+          onClick={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </Button>
+        <ToastsContainer store={ToastsStore} />
+      </div>
     </div>
   );
 }
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-, document.getElementById('root'));
+  // <Provider store={store}>
+  <App />,
+  // </Provider>
+  document.getElementById("root")
+);
