0cfc62f9829105d1cb34c6cb272a73b8263c4604
Bien Thanh Chung <bienthanhchung@luvina.net>
2020-11-13 11:33:50 +0700

    Change file name

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const sass = require('sass');
 
 module.exports = {
-  entry:  ['./src/app.js', './src/common/sass/index.scss'],
+  entry:  ['./src/index.js', './src/common/sass/index.scss'],
   output: {
     filename: 'js/index.js',
     path: path.resolve(__dirname, 'dist')
